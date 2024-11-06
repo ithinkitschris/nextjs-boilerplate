890a86b231576058f3ae272dceb72b9e4049ef39
ithinkitschris <ithinkitschristopher@gmail.com>
2024-11-06 00:02:13 -0500

    6 November 12:02

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 const Navbar = () => {
     return (
         <nav className="
-        fixed top-1 left-5 right-0 p-5 
+        fixed top-2 left-10 right-0 p-5 
         flex justify-center sm:justify-start items-center z-10
-        font-bold tracking-tight">
+        tracking-tight">
             <div className="flex gap-6">
                 <Link href="/"
                 className="hover:text-midground hover:scale-95 transition-all mr-1.5">
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,8 +1,8 @@
 export default function Resume() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 
-        items-start justify-items-center sm:justify-items-start min-h-screen
-        p-10 mt-10 gap-4 
+        items-start justify-items-start min-h-screen
+        p-16 mt-4 gap-4 
         leading-relaxed tracking-tight
         font-[family-name:var(--font-geist-sans)]">
             
@@ -10,13 +10,13 @@ export default function Resume() {
                 <h1 className="-ml-0.5 text-2xl mb-8">
                     Chris Leow
                 </h1>
-                <p className="mb-8">
-                    Email – ithinkitschristopher@gmail.com
-                    <br/>
-                    LinkedIn – <a href="https://www.linkedin.com/in/chris-leow-93372b184/" className="underline transition-all hover:text-midground">Chris Leow</a>
-                    <br/>
-                    Instagram – <a href="https://www.instagram.com/khristurtle/" className="underline transition-all hover:text-midground">@khristurtle</a>
-                </p>
+
+                <h1 className="mb-8 -ml-0.5">
+                Senior Creative<br/>
+                Based in New York City<br/>
+                <i className="-ml-0.5">From Singapore</i>
+                </h1>
+
                 <p className="mb-4">
                     Born and raised in sunny <i>(to put it mildly)</i> Singapore, Chris was once a young kid obsessed with the romanticized image of beret-wearing, palette-wielding artists. Now, he finds himself living the surreal reality of professionally conceptualizing, creating, and directing what is essentially art for the world. 
                     With a fervor for craft and a meticulous eye for finesse, he takes a possibly unhealthy pride in creating visually compelling work across various mediums.
@@ -24,36 +24,84 @@ export default function Resume() {
                 <p className="mb-4">
                     As a multidisciplinary creative and formerly the Creative Lead at ArtScience Museum in Singapore, Chris has eight years of experience in the Advertising and Design industry. He has most notably worked on multiple brand campaigns for Singapore Airlines as an Art Director and global brands the likes of Nike, Samsung, IKEA, Studio Ghibli, Uniqlo and MINI. 
                 </p>
-                <p className="mb-4">
+                <p className="mb-10">
                     In his spare time after work <i>(which, realistically, isn't much)</i>, he does even more work, but for himself—creating content through photography, videography, editing and motion design. When he is finally not working, you will find him thrifting for furniture or, for a more colloquial term, stooping on the streets of New York City. He loves building his living space up, though he wouldn’t go so far as to call it interior design.
+                </p>
+
+                <p className="mb-8">
+                    Email – ithinkitschristopher@gmail.com
+                    <br/>
+                    LinkedIn – <a href="https://www.linkedin.com/in/chris-leow-93372b184/" className="underline transition-all hover:text-midground">Chris Leow</a>
+                    <br/>
+                    Instagram – <a href="https://www.instagram.com/khristurtle/" className="underline transition-all hover:text-midground">@khristurtle</a>
                 </p>
             </div>
 
             <div className="col-span-1 transition-all pr-12">
-                <h1 className="text-2xl mb-8">
+                <h1 className="text-2xl mb-8 -ml-0.5">
                 Currently:<br/>
                 </h1>
                 <p>
-                Graduate Student
+                Master's Student
                 <br/>
                 MFA Interaction Design
                 <br/>
-                School of Visual Arts, New York City
+                School of Visual Arts
                 <br/>
                 <i className="-ml-0.5">September 2024 – May 2026</i>
                 </p>
+
             </div>
             
             <div className="col-span-1 transition-all pr-12">
-                COLUMN 03
+                <h1 className="text-2xl mb-8 -ml-0.5">
+                    Previously:
+                </h1>
+
+                <p>
+                Creative Lead<br/>
+                Marina Bay Sands (ArtScience Museum)<br/>
+                <i className="-ml-0.5">January 2024 – September 2024</i>
+                </p>
+
+                <h1 className="mt-8 mb-2">The World of Studio Ghibli</h1>
+                <p>
+                Creative direction for the integrated marketing campaign for The World of Studio Ghibli, the first ever Studio Ghibli exhibition in Singapore held at ArtScience Museum. Designed the Key Visual for the exhibition and provided creative direction over marketing partners as well as merchandise for the exhibition. 
+                </p>
+                <h1 className="mt-8 mb-2">Goddess: Brave. Bold. Beautiful.</h1>
+                <p>
+                Creative direction and strategic positioning for the marketing campaign of Goddess, an exhibition at the ArtScience Museum that celebrates screen icons across 120 years of moving image history. 
+                </p>
+                <h1 className="mt-8 mb-2">Frida Kahlo: The Life of an Icon</h1>
+                <p>
+                Creative direction and strategic positioning for the marketing campaign of Frida Kahlo: Life of an Icon, an exhibition at the ArtScience Museum on Mexican artist Frida Kahlo, alongside the companion exhibition Laid Bare: Frida’s Inner World. Managed and produced visuals and assets across the campaign titled Frida Forever for the lineup of activities held at the museum.
+                </p>
             </div>
 
-            <div className="tracking-tight text-foreground">
-                COLUMN 04
+            <div className="col-span-1 transition-all pr-12 mt-16">
+                <p>
+                Art Director<br/>
+                TBWA\ Singapore<br/>
+                <i className="-ml-0.5">March 2021 – August 2023</i>
+                </p>
+
+                <h1 className="mt-8">Singapore Airlines</h1>
+                <h1 className="mt-8 mb-4">Beyond The Cabin</h1>
+                <p className="mb-4">
+                Lead Creative and Motion Designer for Beyond The Cabin, an episodic brand campaign and content series that was centred around Singapore Airline’s Cabin Crew’s as World Class travellers with worldly passions.</p>
+                <p>
+                Produced travel guides featuring Cabin Crew, casted for their expertise and passion for destinations flown to by Singapore Airlines. Showcasing a depth of understanding they have for the destinations and novel experiences through 
+                their insights
+                </p>
+
             </div>
 
-            <div className="tracking-tight text-foreground">
-                COLUMN 05
+            <div className="col-span-1 transition-all pr-12 mt-16">
+                <p>
+                Motion Art Director<br/>
+                BBH Singapore<br/>
+                <i className="-ml-0.5">June 2019 – March 2021</i>
+                </p>
             </div>
         </div>
     );
