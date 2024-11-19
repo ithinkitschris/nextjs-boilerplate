4be22adc7909062bc96dac20fd12a3057cd4a4aa
ithinkitschris <ithinkitschristopher@gmail.com>
2024-11-19 12:49:58 -0500

    19 November 12:50pm
    Vercel Deployment attempt #2

--- app/components/photography.js
+++ app/components/photography.js
@@ -9,14 +9,14 @@ const animateIn ={
     hidden: {opacity:0, y:20},
     show: {
         opacity:1, y:0, 
-        transition: {staggerChildren: 0.15, duration:0.5, ease:"easeOut"}
+        transition: {staggerChildren: 0.15, duration:0.5, ease:easeOut}
     }
 }
 const animateInChild ={
 hidden: {opacity:0, y:20},
 show: {
     opacity:1, y:0, 
-    transition: {duration:0.3, ease:"easeOut"}
+    transition: {duration:0.3, ease:easeOut}
     }
 }
 
